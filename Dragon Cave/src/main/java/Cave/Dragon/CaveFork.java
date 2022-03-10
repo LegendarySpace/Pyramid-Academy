c44302e597f14306e55ccf2fe4eddee7b8f1b519
LegendarySpace <40800940+LegendarySpace@users.noreply.github.com>
2022-03-10 15:13:43 -0600

    Implemented path and fork

--- Dragon Cave/src/main/java/Cave/Dragon/CaveFork.java
+++ Dragon Cave/src/main/java/Cave/Dragon/CaveFork.java
@@ -1,2 +1,41 @@
-package Cave.Dragon;public class CaveFork {
+package Cave.Dragon;
+
+import java.util.Scanner;
+
+public class CaveFork extends Cave{
+    Cave left;
+    Cave right;
+    public CaveFork() {
+        if (dist < 3) {
+            left = switch (d10()) {
+                case 1 -> new CaveFork();
+                case 2,3,4,5 -> new CavePath();
+                default -> new CaveDen();
+            };
+            right = switch (d10()) {
+                case 1 -> new CaveFork();
+                case 2,3,4,5 -> new CavePath();
+                default -> new CaveDen();
+            };
+        } else {
+            left = new CaveDen();
+            right = new CaveDen();
+        }
+    }
+
+    @Override
+    public void explore() {
+        dist++;
+        System.out.println("As you make your way through the cave it suddenly widens and you encounter a forking path");
+        Scanner scan = new Scanner(System.in);
+        String direction;
+        do {
+            System.out.println("Which direction do you go? (Left, Right)");
+            direction = scan.next();
+        } while (!direction.equalsIgnoreCase("left") && !direction.equalsIgnoreCase("right"));
+        scan.close();
+
+        if (direction.equalsIgnoreCase("left")) left.explore();
+        else right.explore();
+    }
 }
--- Dragon Cave/src/main/java/Cave/Dragon/CavePath.java
+++ Dragon Cave/src/main/java/Cave/Dragon/CavePath.java
@@ -1,2 +1,28 @@
-package Cave.Dragon;public class CavePath {
+package Cave.Dragon;
+
+public class CavePath extends Cave{
+    Cave connection;
+    public CavePath() {
+        if (dist < 3) {
+            switch (d10()) {
+                case 1, 2, 3, 4 -> connection = new CaveFork();     // 40% chance to spawn fork
+                default -> connection = new CaveDen();
+            }
+        } else {
+            connection = new CaveDen();
+        }
+    }
+
+    @Override
+    public void explore() {
+        dist++;
+        System.out.println("You continue down a long dark corridor. You can barely see and must move slowly.");
+        System.out.println("As you make your way down the twisting path there's an occasional rumble");
+        if (d20() == 1) encounter();        // 5% chance auto game over
+        else connection.explore();
+    }
+
+    public void encounter() {
+        System.out.println("You encounter a wandering dragon and it devours you before you can react. GAME OVER!!!!");
+    }
 }
