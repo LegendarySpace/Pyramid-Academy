f63436f6f25d4b96ee2d33d85faaf6325ec4a6d9
LegendarySpace <40800940+LegendarySpace@users.noreply.github.com>
2022-03-10 14:56:33 -0600

    Implemented abstract cave

--- Dragon Cave/src/main/java/Cave/Dragon/Cave.java
+++ Dragon Cave/src/main/java/Cave/Dragon/Cave.java
@@ -1,2 +1,21 @@
-package Cave.Dragon;public class Cave {
+package Cave.Dragon;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public abstract class Cave {
+    public abstract void explore();
+
+    // increase dist each time an area is explored
+    // if dist is 3 all spawned areas must be dens
+    protected static int dist = 0;
+
+    public static int d20() {
+        return ThreadLocalRandom.current().nextInt(1, 20);
+    }
+    public static int d10() {
+        return ThreadLocalRandom.current().nextInt(1, 10);
+    }
+    public static int d3() {
+        return ThreadLocalRandom.current().nextInt(1, 3);
+    }
 }
