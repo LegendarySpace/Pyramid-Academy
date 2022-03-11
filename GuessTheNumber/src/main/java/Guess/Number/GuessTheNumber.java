2cfe5282cfbfce2a3eef723d4d55b770e1cdad4d
LegendarySpace <40800940+LegendarySpace@users.noreply.github.com>
2022-03-11 03:32:50 -0600

    Finished Guess the number, added unit testing and try catch statements

--- GuessTheNumber/src/main/java/Guess/Number/GuessTheNumber.java
+++ GuessTheNumber/src/main/java/Guess/Number/GuessTheNumber.java
@@ -1,7 +1,84 @@
 package Guess.Number;
 
-public class GuessTheNumber {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
+public class GuessTheNumber {
+    public static Scanner scan;
+    public static int secretNumber;
+    public static int guessAttempts;
+    public static String name;
+
+    public static void main(String[] args) {
+        scan = new Scanner(System.in);
+        guessAttempts = 0;
+        secretNumber = pickRandom();
+
+        enterName();
+        System.out.printf("Welcome %s. I'm thinking of a number between 1 and 20.%n", name);
+
+        boolean correct;
+        do {
+            correct = output(compareGuess(makeGuess()));
+        } while (!correct && guessAttempts <= 6);
+        scan.close();
+
+        if (correct) {
+            System.out.printf("Congratulations %s, you guessed the correct number in %d attempts.%n", name, guessAttempts);
+        } else {
+            System.out.printf("Sorry %s but you have run out of guesses. GAME OVER!!!%n", name);
+        }
+
+    }
+
+    public static int pickRandom() {
+        return ThreadLocalRandom.current().nextInt(1, 20);
+    }
+
+    public static void enterName() {
+        try {
+            System.out.print("Please enter your name: ");
+            name = scan.next();
+        } catch (Exception e) {
+            System.out.println("Error getting player name");
+            e.printStackTrace();
+        }
+    }
+
+    public static int makeGuess() {
+        try {
+            guessAttempts++;
+            int guess;
+            do {
+                System.out.print("Please enter a number between 1 and 20: ");
+                guess = scan.nextInt();
+            } while (guess < 1 || guess > 20);
+            return guess;
+        } catch (Exception e) {
+            System.out.println("Error getting player guess");
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static String compareGuess(int guess) {
+        if (guess == 0) return "invalid";
+        if (guess > secretNumber) return "high";
+        if (guess < secretNumber) return "low";
+        else return "equal";
+    }
+
+    public static boolean output(String value) {
+        if (value.equalsIgnoreCase("equal")) {
+            System.out.println("You guessed correctly!!");
+            return true;
+        } else if (value.equalsIgnoreCase("high")) {
+            System.out.println("Your guess is too high.");
+            return false;
+        } else if (value.equalsIgnoreCase("low")) {
+            System.out.println("Your guess is too low.");
+            return false;
+        }
+        return false;
     }
 }
---
+++ GuessTheNumber/src/test/java/Guess/Number/GuessTheNumberTest.java
@@ -0,0 +1,96 @@
+package Guess.Number;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayInputStream;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Scanner;
+
+class GuessTheNumberTest {
+
+    @Test
+    void pickRandomTest() {
+        for (int i = 0; i < 20; i++) {
+            int x = GuessTheNumber.pickRandom();
+            assertTrue(x >= 1 && x <= 20, "RNG is out of range");
+        }
+    }
+
+    @Test
+    void enterNameTest() {
+        List<String> values = List.of("Brian", "Jordan", "Jacob", "Zach", "Mike", "Luke", "sdfjf", "SDlsucVCIU",
+                "JdfklsKSqwFlkFDlS", "NwefS", "SDfjoefm", "xcVOPem", ".,mwefoi");
+        for (String str : values) {
+            GuessTheNumber.scan = new Scanner(new ByteArrayInputStream(str.getBytes()));
+            GuessTheNumber.enterName();
+            assertEquals(str, GuessTheNumber.name, "Name is not being stored properly");
+            GuessTheNumber.scan.close();
+        }
+    }
+
+    @Test
+    void makeGuessTest() {
+        List<Integer> values = List.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+        for (int i : values) {
+            GuessTheNumber.guessAttempts = 0;
+            GuessTheNumber.scan = new Scanner(new ByteArrayInputStream(BigInteger.valueOf(i).toByteArray()));   // Error converting int to inputstream
+            assertEquals(i, GuessTheNumber.makeGuess(), "Guess is not being correctly returned");
+            GuessTheNumber.scan.close();
+        }
+    }
+
+    @Test
+    void invalidGuessTest() {
+        List<Integer> values = List.of(-1,-97, 65, 52, 21, 34);
+        for (int i : values) {
+            GuessTheNumber.guessAttempts = 0;
+            GuessTheNumber.scan = new Scanner(new ByteArrayInputStream(BigInteger.valueOf(i).toByteArray()));
+            assertEquals(0, GuessTheNumber.makeGuess(), "Guess is not being correctly verified");
+            GuessTheNumber.scan.close();
+        }
+    }
+
+    @Test
+    void wrongGuessTypeTest() {
+        List<String> values = List.of("Brian", "Jordan", "Jacob", "Zach", "Mike", "Luke", "sdfjf", "SDlsucVCIU",
+                "JdfklsKSqwFlkFDlS", "NwefS", "SDfjoefm", "xcVOPem", ".,mwefoi");
+        for (String str : values) {
+            GuessTheNumber.guessAttempts = 0;
+            GuessTheNumber.scan = new Scanner(new ByteArrayInputStream(str.getBytes()));
+            assertEquals(0, GuessTheNumber.makeGuess(), "Incorrect guess data types aren't being handled properly");
+            GuessTheNumber.scan.close();
+        }
+    }
+
+    @Test
+    void compareGuessTest() {
+        List<Integer> values = List.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,-1,-97, 65, 52, 21, 34);
+        for (int i : values) {
+            GuessTheNumber.secretNumber = i;
+            assertEquals("equal", GuessTheNumber.compareGuess(i), "Guess equal comparison is giving incorrect result");
+        }
+        GuessTheNumber.secretNumber = -100;
+        for (int i : values) {
+            assertEquals("high", GuessTheNumber.compareGuess(i), "Guess high comparison is giving incorrect result");
+        }
+        GuessTheNumber.secretNumber = 100;
+        for (int i : values) {
+            assertEquals("low", GuessTheNumber.compareGuess(i), "Guess low comparison is giving incorrect result");
+        }
+        assertEquals("invalid", GuessTheNumber.compareGuess(0), "Guess invalid comparison is giving incorrect result");
+    }
+
+    @Test
+    void outputTest() {
+        assertTrue(GuessTheNumber.output("equal"), "Output equals is giving incorrect result");
+        assertFalse(GuessTheNumber.output("high"), "Output high is giving incorrect result");
+        assertFalse(GuessTheNumber.output("low"), "Output low is giving incorrect result");
+        List<String> values = List.of("Brian", "Jordan", "Jacob", "Zach", "Mike", "Luke", "sdfjf", "SDlsucVCIU",
+                "JdfklsKSqwFlkFDlS", "NwefS", "SDfjoefm", "xcVOPem", ".,mwefoi");
+        for (String str : values) {
+            assertFalse(GuessTheNumber.output(str), "Invalid output is not failing");
+        }
+    }
+}
